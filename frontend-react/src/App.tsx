99051a1a6f1284553adf52aa2997b69e43912c88
ArthurDumoulin <arthur.dum@live.fr>
2022-03-09 14:04:42 +0200

    Ajout structure

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
+import Whiteboard from './views/Whiteboard';
+
 function App() {
   return (
     <div className="App">
       <div className="test">test</div>
+      <Whiteboard />
     </div>
   );
 }
---
+++ frontend-react/src/views/Whiteboard.tsx
@@ -0,0 +1,12 @@
+
+
+function Whiteboard(){
+
+    return(
+        <div>
+            WORK IN PROGRESS ...
+        </div>
+    )
+}
+
+export default Whiteboard
